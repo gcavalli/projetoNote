52fba293e90e6b6b81eedf75594263e2152bc3e0
gleisecavalli <gleisecavalli@gmail.com>
2018-03-15 15:03:54 -0300

    novas classes

---
+++ Tests_NG/src/Test1.java
@@ -0,0 +1,48 @@
+import java.awt.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class Test1{	
+	// Create webDriver reference
+    private WebDriver driver;
+    private static String SEARCH_TEXT = "gleise";
+    private static String SEARCH_PAGE = "https://www.google.com.br";
+    
+	@BeforeTest(alwaysRun = true)
+	// Launch Chrome
+	public void setup () {	
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\gleis\\Downloads\\chromedriver_win32/chromedriver.exe");
+        this.driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(SEARCH_PAGE);
+	}
+		
+	@Test (priority=1)
+	//Do search using google page
+	public void search () throws InterruptedException {	                      		 
+         try {
+	            new WebDriverWait(driver,15).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='lst-ib']"))).sendKeys(SEARCH_TEXT, Keys.ENTER);
+	          } catch (Exception e) {
+	          e.printStackTrace();
+	         }	
+         Thread.sleep(500000);
+
+	}
+	
+	@AfterTest
+	//Close the driver
+	public void close () {              
+	       driver.quit();	   
+	}	    
+}	
+
